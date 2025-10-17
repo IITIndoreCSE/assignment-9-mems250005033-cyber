2c85078a6d0ace4714d0bfd77563490d917f63f1
mems250005033-cyber <mems250005033@iiti.ac.in>
2025-10-17 16:08:27 +0530

    Update Q4.cpp

--- Q4.cpp
+++ Q4.cpp
@@ -1,1 +1,35 @@
-
+#include<iostream>
+#include<string>
+using namespace std;
+struct Salesrecord{
+  string month;
+  float amount;
+};
+int main(){
+    Salesrecord sales[12];
+    cout<<"Enter sales data for each month"<<endl;
+    for(int i=0;i<12;i++){
+        cout<<"Month "<<i+1<<" Name:";
+        cin>>sales[i].month;
+        cout<<"\nSales Amount:";
+        cin>>sales[i].amount;
+        cout<<endl;
+    }
+    float maxAmount=sales[0].amount;
+    float minAmount=sales[0].amount;
+    string maxMonth=sales[0].month;
+    string minMonth=sales[0].month;
+    for(int i=1;i<12;i++){
+        if(sales[i].amount>maxAmount){
+            maxAmount=sales[i].amount;
+            maxMonth=sales[i].month;
+        }
+        if(sales[i].amount<minAmount){
+            minAmount=sales[i].amount;
+            minMonth=sales[i].month;
+        }
+    }
+    cout<<"Month with maximum sales: "<<maxMonth<<" with amount "<<maxAmount<<endl;
+    cout<<"Month with minimum sales: "<<minMonth<<" with amount "<<minAmount<<endl;
+    return 0;
+}
