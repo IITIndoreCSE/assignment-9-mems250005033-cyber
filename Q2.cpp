d9ae477cb5c9e2dad68c96c04f08ac8e6991c828
mems250005033-cyber <mems250005033@iiti.ac.in>
2025-10-17 16:25:41 +0530

    Update Q2.cpp

--- Q2.cpp
+++ Q2.cpp
@@ -1,1 +1,28 @@
+#include <iostream>
+using namespace std;
 
+struct Color {
+    int red;
+    int green;
+    int blue;
+};
+
+int main() {
+    Color colors[5]={
+        {12,40,70},
+        {100,200,120},
+        {70,80,90},
+        {60,95,230},
+        {210,220,190}
+    };
+    for(int i=0; i<3; ++i) {
+        colors[i].red=255-colors[i].red;
+        colors[i].green=255-colors[i].green;
+        colors[i].blue=255-colors[i].blue;
+    }
+    cout<<"New RGB values: "<<endl;
+    for(int i=0; i<5; ++i) {
+        cout<<"Color "<<i+1<<":"<<" R="<<colors[i].red<<" G:"<<colors[i].green<<" B:"<<colors[i].blue<<endl;
+    }
+    return 0;
+}
