e9ff9dd4b350e2915d3a2b505d360095146753f4
mems250005033-cyber <mems250005033@iiti.ac.in>
2025-10-17 16:22:24 +0530

    Update Q3.cpp

--- Q3.cpp
+++ Q3.cpp
@@ -1,1 +1,25 @@
-
+#include<iostream>
+using namespace std;
+struct Point{
+    float x;
+    float y;
+};
+int main(){
+    Point points[7];
+    int count=0;
+    cout<<"Enter coordinates for 7 points (x,y):"<<endl;
+    for(int i=0;i<7;i++){
+        cout<<"Point "<<i+1<<" -x:";
+        cin>>points[i].x;
+        cout<<"\nPoint "<<i+1<<" -y:";
+        cin>>points[i].y;
+        cout<<endl;
+    }
+    for(int i=0;i<7;i++){
+        if(points[i].x>0 && points[i].y>0){
+            count++;
+        }
+    }
+    cout<<"Number of points in the first quadrant: "<<count<<endl;
+    return 0;
+}
